584f97044452992531af9d528c98f6a0edeea551
ddeleon267 <dominique.a.deleon@gmail.com>
2022-07-16 14:18:07 -0400

    Adds comments to illustrate false positives

--- lib/book.py
+++ lib/book.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python3
-
 class Book:
-    pass
+    def __init__(self, title):
+        self.title = title
+
+    def get_author_name(self):
+        return self._author_name
+
+    # def set_author_name(self, author_name):
+    #     self._author_name = author_name
+
+    def get_page_count(self):
+        return self._page_count
+
+    def set_page_count(self, page_count):
+        self._page_count = page_count
+
+    # def get_genre(self):
+    #     return self._genre
+
+    # def set_genre(self, genre):
+    #     self._genre = genre
+
+    
+
+    # author_name = property(get_author_name, set_author_name)
+    page_count = property(get_page_count, set_page_count)
+    # genre = property(get_genre, set_genre)
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    
+    def __init__(self, brand):
+        self.brand = brand
+
+    def cobble(self):
+        self._condition = "New"
+        print("Your shoe is as good as new!")
+
+    def get_condition(self):
+        return self._condition
+    
+    def set_condition(self, condition):
+        self._condition = condition
+
+    condition = property(get_condition, set_condition)
